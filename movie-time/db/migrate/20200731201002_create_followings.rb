7b76e71cb4b4b8db581dc6696a1971e3996d6433
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-01 11:45:56 +0200

    Fix issue with migration

--- movie-time/db/migrate/20200731201002_create_followings.rb
+++ movie-time/db/migrate/20200731201002_create_followings.rb
@@ -1,13 +1,13 @@
 class CreateFollowings < ActiveRecord::Migration[6.0]
   def change
     create_table :followings do |t|
-      t.references :follower, null: false, foreign_key: true
-      t.references :followed, null: false, foreign_key: true
+      t.references :followed, index: true
+      t.references :follower, index: true
 
       t.timestamps
     end
-    add_foreign_keys :followings, :users, column: follower_id
-    add_foreign_keys :followings, :users, column: followed_id
+    add_foreign_key :followings, :users, column: :follower_id
+    add_foreign_key :followings, :users, column: :followed_id
     add_index :followings, [:follower_id, :followed_id], unique:true
   end
 end
---
+++ movie-time/db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_31_201051) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.bigint "followed_id"
+    t.bigint "follower_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_followings_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
+  end
+
+  create_table "opinions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_opinions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fullname"
+    t.string "photo"
+    t.string "coverimage"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "followings", "users", column: "followed_id"
+  add_foreign_key "followings", "users", column: "follower_id"
+  add_foreign_key "opinions", "users"
+end
