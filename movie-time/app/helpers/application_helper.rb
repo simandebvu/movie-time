f08103b506ee21e7c166f64e1dade48a8453440c
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-06 16:57:21 +0200

    Add toastr notifications

--- movie-time/app/helpers/application_helper.rb
+++ movie-time/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def toastr_flash_messages
+        flash.each_with_object([]) do |(type, message), flash_messages|
+          type = 'success' if type == 'notice'
+          type = 'error' if type == 'alert'
+          text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
+          flash_messages << text.html_safe if message
+        end.join("\n").html_safe
+    end
 end
