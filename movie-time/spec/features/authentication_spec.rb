eb003553c1f1b92268b9f4c155b80236cca93363
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-07 10:09:34 +0200

    Add Upload

--- movie-time/spec/features/authentication_spec.rb
+++ movie-time/spec/features/authentication_spec.rb
@@ -1,14 +1,37 @@
 require 'rails_helper'
 RSpec.describe 'login followed by logout', type: :feature do
   feature 'Signing up' do
-    scenario 'sign up user' do
-      user1 = User.create(fullname: 'zack brannigan', username: "leelaissafe",email: 'zackleela@te.com', password: 'password', password_confirmation: 'password', photo: 'sdjhg', cover_image: 'hsgdjhgsd')
-      user2 = User.create(fullname: 'hypnotoad', username: 'rootuser', email: 'hypnosis@te.com', password: 'password', password_confirmation: 'password', photo: 'sdjhg', cover_image: 'hsgdjhgsd')
-      visit new_user_session_path
-      fill_in 'user[email]', with: 'zackleela@te.com'
-      fill_in 'user[password]', with: 'password'
-      click_button 'Log in'
-      expect(page).to have_content('Signed in successfully.')
+    before(:each) do
+        @user = User.new
+        @user.fullname = 'Leela'
+        @user.username = 'testuser'
+        @user.password = 'leelafry'
+        @user.email = 'good@mail.com'
+        @user.password_confirmation = 'leelafry'
+        File.open(File.expand_path('spec/bender.jpg')) do |f|
+            user.cover_image = f
+            user.photo = f
+        end
+        @user.save
+        @user2 = User.new
+        @user2.fullname = 'Leela2'
+        @user2.username = 'testuser2'
+        @user2.password = 'leelafry2'
+        @user2.email = 'good@mail.com2'
+        @user2.password_confirmation = 'leelafry2'
+        File.open(File.expand_path('spec/bender.jpg')) do |f|
+            user.cover_image = f
+            user.photo = f
+        end
+        @user2.save
+        visit new_user_session_path
+        fill_in 'user_email', with: 'zackleela@te.com'
+        fill_in 'user_password', with: 'password'
+        click_button 'Log in'
+    end
+
+    scenario 'sign in' do
+      expect(page).to have_content('Lets opinionate!')
       click_on 'Logout'
       expect(page.current_path).to eq new_user_session_path
     end
