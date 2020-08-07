3edd7c69db780f1218159d9a37f2a94c5f0fbfb4
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-07 10:45:00 +0200

    Update GitIgnore

--- movie-time/spec/features/authentication_spec.rb
+++ movie-time/spec/features/authentication_spec.rb
@@ -1,39 +1,67 @@
 require 'rails_helper'
+
 RSpec.describe 'login followed by logout', type: :feature do
-  feature 'Signing up' do
-    before(:each) do
-        @user = User.new
-        @user.fullname = 'Leela'
-        @user.username = 'testuser'
-        @user.password = 'leelafry'
-        @user.email = 'good@mail.com'
-        @user.password_confirmation = 'leelafry'
-        File.open(File.expand_path('spec/bender.jpg')) do |f|
-            user.cover_image = f
-            user.photo = f
-        end
-        @user.save
-        @user2 = User.new
-        @user2.fullname = 'Leela2'
-        @user2.username = 'testuser2'
-        @user2.password = 'leelafry2'
-        @user2.email = 'good@mail.com2'
-        @user2.password_confirmation = 'leelafry2'
-        File.open(File.expand_path('spec/bender.jpg')) do |f|
-            user.cover_image = f
-            user.photo = f
-        end
-        @user2.save
-        visit new_user_session_path
-        fill_in 'user_email', with: 'zackleela@te.com'
-        fill_in 'user_password', with: 'password'
-        click_button 'Log in'
+  let(:username) { 'someperson' }
+  let(:email) { 'teset@mail.com' }
+  let(:fullname) { 'Decsiobs ashd' }
+  let(:password) { 'password' }
+
+  context 'user unsigned in' do
+    before do
+      visit new_user_session_path
+      fill_in 'Email', with: 'someone@mail.com'
+      fill_in 'Password', with: '320103'
+      click_button 'Log in'
     end
 
-    scenario 'sign in' do
-      expect(page).to have_content('Lets opinionate!')
-      click_on 'Logout'
-      expect(page.current_path).to eq new_user_session_path
+    it 'displays an error message' do
+      expect(page).to have_content('Invalid Email or password.')
+    end
+
+    it 'shows the correct menu links' do
+      within('.navbar') do
+        expect(page).to have_link('Sign In')
+        expect(page).to have_link('Sign Up')
+      end
+    end
+  end
+
+  context 'user sign in' do
+    before do
+      visit new_user_session_path
+      fill_in 'Email', with: email
+      fill_in 'user_password', with: password
+      click_button 'Log in'
+    end
+
+    it 'shows a hello message' do
+      expect(page).to have_content('Signed in successfully.')
+    end
+
+    it 'shows the correct menu links' do
+      within('.user-nav') do
+        expect(page).to_not have_link('Log In')
+        expect(page).to_not have_link('Register')
+      end
+
+      within('.user-nav') do
+        expect(page).to have_link('Movie Time')
+        expect(page).to have_link('Logout')
+      end
+    end
+
+    it 'create a valid post' do
+      fill_in 'post_text', with: 'Hello guys it\'s me happy to be here.'
+      page.execute_script("document.querySelector('form#new_post').submit()")
+
+      expect(page).to have_content('post was created successfuly.')
+    end
+
+    it 'create a none valid post' do
+      fill_in 'post_text', with: 'Hi'
+      page.execute_script("document.querySelector('form#new_post').submit()")
+
+      expect(page).to have_content('post was not created please try again.')
     end
   end
 end
