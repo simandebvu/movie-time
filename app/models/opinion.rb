f2783441921531a6678bf851e97f68ced957aecd
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-09 17:39:40 +0200

    Refactor profile and stats

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,5 +2,6 @@ class Opinion < ApplicationRecord
   belongs_to :user
 
   validates :text, presence: true, length: { maximum: 145 }
-
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  
 end
